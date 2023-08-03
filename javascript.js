183aa0800620d292d5333788ffeca3026a6b6123
ArminasAcas <arminasprollas@gmail.com>
2023-08-03 23:56:43 +0300

    Made image transition be more smooth

--- javascript.js
+++ javascript.js
@@ -2,28 +2,35 @@ document.addEventListener("DOMContentLoaded" , function () {
 
     let buttons = document.querySelectorAll(".buttons");
     let images = document.querySelectorAll(".image");
+    
     if (!buttons || !images) return;
 
     images.forEach(image => {
         image.style.left = 0 + "px";
     })
 
+    let interval;
     buttons.forEach(button => {
-        button.addEventListener("click", function() {
-            if (button.innerHTML == "&lt;")
-            {
-                images.forEach(image => {
-                    console.log(image.style.left);
-                    console.log(parseInt(image.style.left)); 
-                    image.style.left = parseInt(image.style.left) + 20 + "px";
-                })
-            } 
-            else if (button.innerHTML == "&gt;")
-            {
-                images.forEach(image => {
-                    image.style.left = parseInt(image.style.left) - 20 + "px"; 
-                })
-            }  
+        button.addEventListener("mousedown", function() {
+           interval = setInterval(() => checkButtonClick(button, images), 5);
+        })
+        button.addEventListener("mouseup", function() {
+            clearInterval(interval);
         })
     })
 })
+
+function checkButtonClick(button, images) {
+    if (button.innerHTML == "&lt;")
+        {
+        images.forEach(image => {
+                image.style.left = parseInt(image.style.left) + 2 + "px";
+            })
+        } 
+    else if (button.innerHTML == "&gt;")
+        {
+            images.forEach(image => {
+                image.style.left = parseInt(image.style.left) - 2 + "px"; 
+            })
+        } 
+}
