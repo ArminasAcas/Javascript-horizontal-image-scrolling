90de7f882dc55c018078254cf1089a00bb56d09c
ArminasAcas <arminasprollas@gmail.com>
2023-08-04 23:09:54 +0300

    fixed bugs and compressed images

---
+++ javascript/main.js
@@ -0,0 +1,47 @@
+document.addEventListener("DOMContentLoaded" , function () {
+
+    let buttons = document.querySelectorAll(".buttons");
+    let images = document.querySelectorAll(".image");
+    
+    if (!buttons || !images) return;
+
+    preloadImages(images);
+
+    images.forEach(image => {
+        image.style.left = 0 + "px";
+    })
+
+    let interval;
+    buttons.forEach(button => {
+        button.addEventListener("mousedown", function() {
+           interval = setInterval(() => checkButtonClick(button, images), 5);
+        })
+        button.addEventListener("mouseup", function() {
+            clearInterval(interval);
+        })
+        button.addEventListener("mouseleave", function() {
+            clearInterval(interval);
+        })
+    })
+})
+
+function checkButtonClick(button, images) {
+    if (button.innerHTML == "&lt;")
+        {
+        images.forEach(image => {
+                image.style.left = parseInt(image.style.left) + 3 + "px";
+            })
+        } 
+    else if (button.innerHTML == "&gt;")
+        {
+            images.forEach(image => {
+                image.style.left = parseInt(image.style.left) - 3 + "px"; 
+            })
+        } 
+}
+
+function preloadImages(images){
+    images.forEach((image,index) => {
+        image.setAttribute('src','images/' + imgSource[index]);
+    })
+}
