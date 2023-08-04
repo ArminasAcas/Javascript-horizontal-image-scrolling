ac2a0bbb04a9c2bc12a186cd74a38d988040306f
ArminasAcas <arminasprollas@gmail.com>
2023-08-04 23:47:52 +0300

    added horizontal image scrolling limits

--- javascript/imageScroll.js
+++ javascript/imageScroll.js
@@ -24,16 +24,19 @@ function setUpImageScroll() {
 }
     
 function checkButtonClick(button, images) {
+
     if (button.innerHTML == "&lt;")
         {
         images.forEach(image => {
-                image.style.left = parseInt(image.style.left) + 3 + "px";
+                if(parseInt(image.style.left) < 0)image.style.left = parseInt(image.style.left) + 3 + "px";
             })
         } 
     else if (button.innerHTML == "&gt;")
         {
+
+            let imagesWidth = calculateImagesWidthSum(images);
             images.forEach(image => {
-                image.style.left = parseInt(image.style.left) - 3 + "px"; 
+                if(parseInt(image.style.left) > imagesWidth * -1)image.style.left = parseInt(image.style.left) - 3 + "px";
             })
         } 
     }
@@ -42,4 +45,13 @@ function preloadImages(images){
     images.forEach((image,index) => {
         image.setAttribute('src','images/' + imgSource[index]);
     })
+}
+
+function calculateImagesWidthSum(images) {
+    let width = 0;
+    images.forEach((image, index) => {
+        if (imgSource.length - 1 !== index)width += parseInt(window.getComputedStyle(image).width);
+    })
+    width -= (imgSource.length - 1) * 50 + 25;
+    return width;
 }
