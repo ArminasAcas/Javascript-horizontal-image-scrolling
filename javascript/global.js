ea55d7122c6f3269fd22985a63b7b5ac2d63af0e
ArminasAcas <arminasprollas@gmail.com>
2023-08-06 23:01:43 +0300

    js code changes and image dimension reduction

--- javascript/global.js
+++ javascript/global.js
@@ -1,1 +1,3 @@
 let imgSource = ["img1.jpg", "img2.jpg", "img3.jpg","img4.jpg"];
+let scrollSpeed = 3;
+let images = null;
--- javascript/imageScroll.js
+++ javascript/imageScroll.js
@@ -1,9 +1,9 @@
 function setUpImageScroll() {
     let buttons = document.querySelectorAll(".button");
-    let images = document.querySelectorAll(".image-block__images");
+    images = document.querySelectorAll(".image-block__images");
     if (!buttons || !images) return;
 
-    preloadImages(images);
+    preloadImages();
 
     images.forEach(image => {
         image.style.left = 0 + "px";
@@ -12,46 +12,45 @@ function setUpImageScroll() {
     let interval;
     buttons.forEach(button => {
         button.addEventListener("mousedown", function() {
-           interval = setInterval(() => checkButtonClick(button, images), 5);
+           interval = setInterval(() => checkButtonClick(button), 5);
         })
         button.addEventListener("mouseup", function() {
-            clearInterval(interval);
+            if (interval) clearInterval(interval);
         })
         button.addEventListener("mouseleave", function() {
-            clearInterval(interval);
+            if (interval) clearInterval(interval);
         })
     })
 }
     
-function checkButtonClick(button, images) {
+function checkButtonClick(button) {
 
     if (button.innerHTML == "&lt;")
         {
         images.forEach(image => {
-                if(parseInt(image.style.left) < 0)image.style.left = parseInt(image.style.left) + 3 + "px";
+                if(parseInt(image.style.left) < 0)image.style.left = parseInt(image.style.left) + scrollSpeed + "px";
             })
         } 
     else if (button.innerHTML == "&gt;")
         {
-
-            let imagesWidth = calculateImagesWidthSum(images);
+            let imagesWidth = calculateImagesWidthSum();
             images.forEach(image => {
-                if(parseInt(image.style.left) > imagesWidth * -1)image.style.left = parseInt(image.style.left) - 3 + "px";
+                if(parseInt(image.style.left) > imagesWidth * -1)image.style.left = parseInt(image.style.left) - scrollSpeed + "px";
             })
         } 
     }
     
-function preloadImages(images){
+function preloadImages(){
     images.forEach((image,index) => {
         image.setAttribute('src','images/' + imgSource[index]);
     })
 }
 
-function calculateImagesWidthSum(images) {
+function calculateImagesWidthSum() {
     let width = 0;
     images.forEach((image, index) => {
         if (imgSource.length - 1 !== index)width += parseInt(window.getComputedStyle(image).width);
     })
-    width -= (imgSource.length - 1) * 50 + 25;
+    width -= (imgSource.length - 1) * 20;
     return width;
 }
